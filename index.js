b33bfd8fddfe3ba637537aac3b0241c1d0991944
Yasuhiko Nara <narayasuhiko3@gmail.com>
2020-10-22 13:41:11 +0900

    made simple docker app

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-  console.log('hello');
+  res.send('replying from container!').status(200);
 });
 
 app.listen(3000, () => {
-  console.log('listening on port 3000');
+  console.log('hello from container!, listening on port 3000');
 });
