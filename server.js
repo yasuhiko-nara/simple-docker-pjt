a1e1291737d45377bc95f3364c29d056802c49b3
Yasuhiko Nara <narayasuhiko3@gmail.com>
2020-10-22 17:36:55 +0900

    succeessed to implement nginx

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => {
-  res.send('hello from port 4000').status(200);
-});
-
-app.listen(4000, () => {
-  console.log('listening on port 4000');
-});
